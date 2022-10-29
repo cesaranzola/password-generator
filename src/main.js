7716e258ebb11d75d32e5e8334f3d38301e55310
deus <deus@192.168.1.7>
2022-10-29 09:35:47 -0500

    Add a varibale password length

--- src/main.js
+++ src/main.js
@@ -102,9 +102,8 @@ let passOne = document.getElementById('pass-one');
 let passTwo = document.getElementById('pass-two');
 let firstSpan = document.getElementById('first-span');
 let secondSpan = document.getElementById('sd-span');
-// firstSpan.style.display = 'none';
-// secondSpan.style.display = 'none';
-let sliderAmount = document.getElementById('slider-amount');
+let sliderAmount = parseInt(document.getElementById('slider-amount').value);
+let getLower = document.getElementById('lower').value;
 
 let arrLen = characters.length - 1;
 let randNum1 = 0;
@@ -113,11 +112,13 @@ let pass1 = [];
 let pass2 = [];
 
 function generatePass() {
+	console.log(typeof getLower) ;
+
 	pass1 = [];
 	pass2 = [];
 	passOne.textContent = '';
 	passTwo.textContent = '';
-	for (let i = 0; i < 15; i++) {
+	for (let i = 0; i < sliderAmount; i++) {
 		randNum1 = Math.floor(Math.random() * arrLen) + 1;
 		randNum2 = Math.floor(Math.random() * arrLen) + 1;
 		pass1.push(characters[randNum1]);
@@ -127,55 +128,8 @@ function generatePass() {
 
 function setPass() {
 	generatePass();
-	for (let i = 0; i < 15; i++) {
+	for (let i = 0; i < sliderAmount; i++) {
 		passOne.textContent += pass1[i];
 		passTwo.textContent += pass2[i];
 	}
-}
-
-// function copyTxt() {
-// 	// Get the text field
-// 	// let copyText = document.getElementById('myInput');
-
-// 	passOne.addEventListener('click', function () {
-// 		// passOne[0].select();
-// 		// passOne.setSelectionRange(0, 99999);
-// 		// passOne.textContent = navigator.clipboard.writeText(passOne.value);
-// 		passOne.textContent = 'pamplinas';
-// 	});
-
-// passTwo.addEventListener('click', function () {
-// 	passTwo.select();
-// 	passTwo.setSelectionRange(0, 99999);
-// 	passTwo.textContent = navigator.clipboard.writeText(passTwo.value);
-// });
-
-// Select the text field
-// For mobile devices
-
-// Copy the text inside the text field
-// }
-
-function copyTxt() {
-	passOne.one(
-		addEventListener('click', function () {
-			// passOne[0].select();
-			// passOne.setSelectionRange(0, 99999);
-			// navigator.clipboard.writeText(passOne.value);
-			passOne.style.fontSize = '0.6rem';
-			passOne.textContent = 'Copy to the clipboard';
-		})
-	);
-}
-
-sliderAmount.rendered = function () {
-	document.getElementById('slider-amount').oninput = function () {
-		myFunction();
-	};
-};
-
-function myFunction() {
-	let val = document.getElementById('slider-amount').value; //gets the oninput value
-	document.getElementById('output').innerHTML = val; //displays this value to the html page
-	console.log(val);
 }
